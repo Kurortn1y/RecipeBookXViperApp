49200ce3364d58c0ed9a635b0ecad47c2087761e
kurortn1ty <lastmandanc1ng@icloud.com>
2024-08-31 14:25:34 +0300

    Initial Commit

---
+++ RecipeBookXViperApp/RecipeBookXViperAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBookXViperAppApp.swift
+//  RecipeBookXViperApp
+//
+//  Created by Roman Kondrashov on 31.08.24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeBookXViperAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
